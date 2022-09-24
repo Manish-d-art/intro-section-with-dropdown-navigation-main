b3bcbd5bdf44ca24386d738f9e33f4f27da501ae
manish mandal <76776194mm@gmail.com>
2022-09-24 22:34:49 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@
 const navOpenBtn=document.querySelector('.menuBtn');
 const navbar=document.querySelector('.nav-list');
 const closeNavBtn=document.querySelector('.closeBtn');
+const mainSec=document.querySelector('.main');
+const Body=document.querySelector('body');
 
 navOpenBtn.addEventListener('click',function(e){
   e.preventDefault();
   navbar.style.transform=`translateX(${0}%)`;
+
 });
 
 closeNavBtn.addEventListener('click',function(e){
