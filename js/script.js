42dab98fe61454127aeb6f1c259304792d55954f
manish mandal <76776194mm@gmail.com>
2022-09-24 22:10:07 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@
 'use strict'
 
 const navOpenBtn=document.querySelector('.menuBtn');
+const navbar=document.querySelector('.nav-list');
 
+navOpenBtn.addEventListener('click',function(e){
+  e.preventDefault();
+  
+})
