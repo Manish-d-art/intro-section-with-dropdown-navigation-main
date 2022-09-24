3368f5a0c1cbb85de206b3f99c9c0c3dd13dffe4
manish mandal <76776194mm@gmail.com>
2022-09-24 22:37:26 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -10,11 +10,15 @@ const Body=document.querySelector('body');
 navOpenBtn.addEventListener('click',function(e){
   e.preventDefault();
   navbar.style.transform=`translateX(${0}%)`;
+  mainSec.style.opacity=".4";
+  Body.style.backgroundColor="rgba(0, 0, 0,60%)";
 
 });
 
 closeNavBtn.addEventListener('click',function(e){
   e.preventDefault();
   navbar.style.transform=`translateX(${107}%)`;
+  mainSec.style.opacity="1";
+  Body.style.backgroundColor="white";
 })
 
