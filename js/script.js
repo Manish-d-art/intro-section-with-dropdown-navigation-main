dbc7fe2f453d57d4bd7397dd961c96579a6f31f2
manish mandal <76776194mm@gmail.com>
2022-09-24 22:18:02 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -6,5 +6,6 @@ const navbar=document.querySelector('.nav-list');
 
 navOpenBtn.addEventListener('click',function(e){
   e.preventDefault();
-  
+  navbar.style.transform=`translateX(${0}%)`;
+  // console.log(1);
 })
